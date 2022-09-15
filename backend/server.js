b91fdc5b44f19407f6e77c6ddf5220700af78121
sali <sashalischinskiy72@gmail.com>
2022-09-15 12:35:26 +0300

    Remove get route

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,13 @@ import express from "express";
 import cors from "cors";
 import users from './api/routers.js'
 const app = express();
+import cookieParser from 'cookie-parser';
 
 // Middleware
-app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/v1", users);
-
-app.get('/', function(req, res){
-    res.redirect('/api/v1/sign-up');
-});
 
 export default app

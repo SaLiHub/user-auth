587d98e6e24ea7285489d3929b039ddfa4f3b8e2
sali <sashalischinskiy72@gmail.com>
2022-09-15 12:30:25 +0300

    Add demo component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,25 @@ import React from 'react';
 
 import './App.css';
 
-import {Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import SignIn from "./auth/SignIn";
 import SignUp from './auth/SignUp';
+import Profile from "./components/Profile";
 
 // const SignUp = React.lazy(() => import('./auth/SignUp'));
 // const SignIn = React.lazy(() => import('./auth/SignIn'));
 
-console.log('lukisadfasd')
 function App() {
   return (
       <div className="App">
         <Routes>
           <Route path="/sign-in" element={<SignIn/>}/>
           <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route
+          path="/"
+          element={<Navigate to="/sign-in" replace />}
+          />
         </Routes>
       </div>
   );

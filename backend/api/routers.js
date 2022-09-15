e77738c4109a5828627e9a847035b132d38c04de
sali <sashalischinskiy72@gmail.com>
2022-09-15 12:33:55 +0300

    Add middleware and controller for routes

--- backend/api/routers.js
+++ backend/api/routers.js
@@ -6,6 +6,11 @@ const router = express.Router();
 
 router.route("/sign-up").post(UsersController.apiPostUser);
 
+router.route("/sign-in").post(UsersController.apiValidateUser);
+
+router.route("/profile").get(UsersController.apiAuthenticateToken, UsersController.apiGetUser);
+
+
 // router.route("/check").get((req, res,) => res.send('check'));
 //
 //
@@ -31,7 +36,7 @@ router.route("/sign-up").post(UsersController.apiPostUser);
 //     const newTask = new Task({isChecked, inputValue});
 //     newTask.save()
 //         .then(() => res.json(newTask))
-//         .catch(err => res.status(400).json('Error: ' + err));
+//         .catch(err => res.status(400).json('Alert: ' + err));
 // });
 
 export default router;
